999234c6b9a6dcbc6e04699e58fba2f1355a2dcb
Jinwoo Seo <sjw1980@hotmail.com>
2022-08-16 14:09:17 +0000

    Adding github action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️ for Jinwoo
+            Docusaurus Tutorial - 5min ⏱️ for Jinwoo Seo!!
           </Link>
         </div>
       </div>
